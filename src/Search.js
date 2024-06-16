b32638d4f31d005b76ff648965e5a7fb14d135e7
Jennygreg <jennygregory11@gmail.com>
2024-06-16 14:23:12 +0100

    updated Search component

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React,  {useState} from 'react';
 import './Search.css'
+
 export default  function Search(){
+    const [city, setCity ]=useState("null"); 
+    function searchHandle(){
+       let searchCity = {city};
+       console.log(searchCity)
+    }
+    function City(event){
+        setCity(event.target.value);
+    }
+    
 return (<div className='Search'>
-<form>
-    <input type='Search' placeholder='Search  a country' className='searchInput'/>
+<form onClick={searchHandle}>
+    <input type='Search' placeholder='Search  a country' className='searchInput' onChange={City}/>
     <input type='submit'value='search' className='Submit'/>
 </form>
 </div>)
