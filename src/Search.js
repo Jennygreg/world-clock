4b76a17632ef8329fdc1bf3111fc6e9141cf4d6e
Jennygreg <jennygregory11@gmail.com>
2024-11-23 22:26:35 +0100

    updated Time.js and removed error varible in search.js

--- src/Search.js
+++ src/Search.js
@@ -7,8 +7,9 @@ import FavCity from './FavCity.js';
 export default  function Search(){
     const [city, setCity ]=useState(''); 
     const[data, setData]=useState({loaded:false});
-    const [errorMessage, setErrorMessage]=useState(null)
+   
         
+    
     const searchHandle= async(city)=>{
             
             try{
@@ -21,48 +22,24 @@ export default  function Search(){
             return response
          } 
          catch(error){
-            if(error.message.includes(429)){
-                setErrorMessage('Too Many Request, Please Wait for 2mins')    
-            }
-            else if (error.message.includes('Failed to fetch')){
-                setErrorMessage(<div><p>Cannot find city/country: Please enter a valid search input eg Africa/Lagos, Europe/London..</p>
-                <p>Network Error: Also check your Connection and try again</p></div>)
-            }else{
-                setErrorMessage(error.message )
-            }
+            
             console.log(error.message)
        }
         };  
+       
       async function SearchInput(e){ 
             e.preventDefault()
           const response= await searchHandle(city)
           console.log(response)
            setData({loaded:true, response})
+          
             }
     function City(event){   
         setCity(event.target.value);
     }
      
- if (errorMessage !==null ){
-    return(
-        <div className="Search" id="container">
-          <form onSubmit={SearchInput}>
-            <input
-              type="text"
-              placeholder="Search a country"
-              className="searchInput"
-              value={city}
-              onChange={City}
-              name='Searchvalue}'
-             id="searchInput"
-            />
-            <input type="submit" value="search" className="Submit" />
-          </form>
-          <div style={{ color: 'black' }}>{errorMessage}</div>
-      <FavCity/>
-    </div>)
-        
-       } else if (data.loaded===true) { return (
+ 
+      if(data.loaded===true) {return (
         <div className="Search" id="container">
           <form onSubmit={SearchInput}>
             <input
@@ -76,8 +53,9 @@ export default  function Search(){
             />
             <input type="submit" value="search" className="Submit" />
           </form>
-            <Time myData={data.response} />
-            <FavCity/></div>)}
+            <Time myData={data} />
+            <FavCity/></div>)} 
+      
         else{
            return(
             <div className="Search" id="container">
@@ -90,7 +68,7 @@ export default  function Search(){
                    onChange={City}
                    name='Searchvalue}'
                   id="searchInput"
-                  autoFocus={false}
+                 
                 />
                 <input type="submit" value="search" className="Submit" />
               </form>
--- src/Time.js
+++ src/Time.js
@@ -1,48 +1,46 @@
 import React from 'react';
+
 import './Time.css'
 export default function Time(props){
-   const myData=props.myData
-   if(!myData){
-    return<div>Loading please wait...</div>
-   }
-console.log(props)
-const Time= myData.datetime;
-const Timezone=myData.timezone;
-const offset=myData.utc_offset;
-const abbr=myData.abbreviation;
+const timeDate=props.myData.response; 
 
-const Days=['Monday','Tuesday','Wednesday','Thursday', 'Friday','Saturday','Sunday'];
-const Months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-const myDate= new Date(Time);
-console.log(myDate);
-const day=Days[myDate.getDay()];
-const Month=Months[myDate.getMonth()]
-const Year = myDate.getFullYear()
-let second =myDate.getUTCSeconds().toString();
-if(second< 10){
-    second=`0${second}`;
-}
-let min=myDate.getUTCMinutes().toString();
-if (min<10){
-    min=`0${min}`
-}
-let hour = myDate.getUTCHours().toString();
-if(hour<0){
-    hour=`0${hour}`
-}
-let currentTime=`${hour}:${min}:${second}`;
-let currentDate=`${day} ${Month},${Year} `;
-let utcTimezoneOffSet = `UTC:${offset}`;
-
-return(
-    <div>
-        <h1>{Timezone}</h1>
-        <h2> {currentDate}</h2>
-        <p>{currentTime}
-        <span> {utcTimezoneOffSet}</span>
-        <span> {abbr}</span>
-        </p> 
-    </div>)
+if(timeDate){console.log(props)
+    const {datetime,timezone,utc_offset, abbreviation}= timeDate
+    const Days=['Monday','Tuesday','Wednesday','Thursday', 'Friday','Saturday','Sunday'];
+    const Months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    const myDate= new Date(datetime);
+    console.log(myDate);
+    const day=Days[myDate.getDay()];
+    const Month=Months[myDate.getMonth()]
+    const Year = myDate.getFullYear()
+    let second =myDate.getUTCSeconds().toString();
+    if(second< 10){
+        second=`0${second}`;
+    }
+    let min=myDate.getUTCMinutes().toString();
+    if (min<10){
+        min=`0${min}`
+    }
+    let hour = myDate.getUTCHours().toString();
+    if(hour<0){
+        hour=`0${hour}`
+    }
+    let currentTime=`${hour}:${min}:${second}`;
+    let currentDate=`${day} ${Month},${Year} `;
+    let utcTimezoneOffSet = `UTC:${utc_offset}`;
+       
+    return(
+        <div>
+            <h1>{timezone}</h1>
+            <h2> {currentDate}</h2>
+            <p>{currentTime}
+            <span> {utcTimezoneOffSet}</span>
+            <span> {abbreviation}</span>
+            </p> 
+        </div>)}else{
+    
+    return 'Please check your network connection and enter valid search e.g Africa/Lagos, Europe/London '
+ }
 
 
 }
