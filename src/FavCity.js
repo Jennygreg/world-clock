651ebb86cec6481371f8a268972a380f488b2f5a
Jennygreg <jennygregory11@gmail.com>
2025-01-18 19:36:32 +0100

    added a netlify toml file

--- src/FavCity.js
+++ src/FavCity.js
@@ -24,7 +24,8 @@ export default function FavCity(){
 
             }})
             const timezoneData=  await Promise.all(cordinates.map((cityCordinates)=>
-              fetch(` http://api.geonames.org/timezoneJSON?&lat=${cityCordinates.lat}&lng=${cityCordinates.lon}&username=mizbella`)
+              fetch(` 
+/api.geonames.org/timezoneJSON?&lat=${cityCordinates.lat}&lng=${cityCordinates.lon}&username=mizbella`)
             ))
          const timeZoneResponse= await Promise.all(timezoneData.map((response)=>response.json()))
           setCityData (timeZoneResponse)
